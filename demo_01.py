32e7348674ac7abdb2a965bd52dc64b3cc18c860
sayan <sayantankar01@gmail.com>
2022-08-28 22:55:44 +0530

    Initial commit

---
+++ demo_01.py
@@ -0,0 +1,22 @@
+a = input("Please enter a number:")
+b = input("Please enter second number:")
+
+print("Your numbers are",a ,"and",b)
+
+c = a,b
+c = a,b
+print("The sum is:",a,b) # This is concatination
+print("The sum is:",int(a),b) # This is concatination
+# print("The sum is:",int(a)+b) # This is error
+print("The sum is:",c) # This is concatination
+
+
+# d = int(a)+int(b)
+# print("The sum is: ",d)
+
+e= bool(input("Enter: "))
+
+print(e)
+
+a=False
+print(a)
